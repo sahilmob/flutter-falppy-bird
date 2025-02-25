4fa889030f975e57b153151b2f36dc5c6b547010
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-02-25 23:05:16 +0300

    add background

--- lib/game.dart
+++ lib/game.dart
@@ -1,14 +1,19 @@
 import 'dart:async';
 import 'package:flame/game.dart';
 import 'package:flame/events.dart';
+import 'package:flappy_bird/components/background.dart';
 
 import 'package:flappy_bird/components/bird.dart';
 
 class FlappyBirdGame extends FlameGame with TapDetector {
   late Bird bird;
+  late Background background;
 
   @override
   FutureOr<void> onLoad() {
+    background = Background(size);
+    add(background);
+
     bird = Bird();
     add(bird);
   }
